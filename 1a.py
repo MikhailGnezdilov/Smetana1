a5310e6ef12871f375f9121799927ef84709cbbf
MikhailGnezdilov <161834916+MikhailGnezdilov@users.noreply.github.com>
2024-03-15 14:11:32 +0300

    Create 1a.py

---
+++ 1a.py
@@ -0,0 +1,20 @@
+from math import *
+x=2
+y=2
+z=2
+t=2
+r1=(x**y)**(t*z)-(e**(3*x))
+print('r1=', r1)
+x=2
+t=2
+r2=(sqrt(0.3*t*x))+(1/(tan(x/2)**2))
+print('r2=', r2)
+x=0.05
+r3=((x+3)/(3*x))**(1/7)+((cos(5*x))**3) 
+print('r3=', r3)
+x=1
+y=2
+z=3
+t=3
+r4=(8*(abs(x*y))/(3*t*z))-(log1p(x+1))**(1/3)
+print('r4=', r4)
